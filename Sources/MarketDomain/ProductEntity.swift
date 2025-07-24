13c3eace1928b3a3e278e9e47ee28bf51c270e3f
Onur Yılmaz <ozelonuryilmaz@hotmail.com>
2025-07-25 01:17:38 +0300

    chore: expose public init for App layer components

--- Sources/MarketDomain/ProductEntity.swift
+++ Sources/MarketDomain/ProductEntity.swift
@@ -8,4 +8,9 @@
 public struct ProductEntity {
     public let image: String
     public let name: String
+    
+    public init(image: String, name: String) {
+        self.image = image
+        self.name = name
+    }
 }
