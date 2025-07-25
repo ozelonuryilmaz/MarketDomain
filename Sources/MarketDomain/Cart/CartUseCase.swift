5ed8777196f8051436c39cc8bc124115c31a8f2a
Onur Yılmaz <ozelonuryilmaz@hotmail.com>
2025-07-25 18:59:43 +0300

    +++

--- Sources/MarketDomain/Cart/CartUseCase.swift
+++ Sources/MarketDomain/Cart/CartUseCase.swift
@@ -21,11 +21,13 @@ public final class CartUseCase: ICartUseCase {
         self.cartRepository = cartRepository
     }
 
+    // TODO: addToCart and getCartItems functions do not follow UseCase best practices. Fixed in App layer, but UseCases need refactoring accordingly.
     public func addToCart(_ item: CartEntity) -> Bool {
         let dto = ProductCartDTO(id: item.id, name: item.name, quantity: item.quantity, price: item.price)
         return cartRepository.saveCartItem(dto)
     }
 
+    // TODO: addToCart and getCartItems functions do not follow UseCase best practices. Fixed in App layer, but UseCases need refactoring accordingly.
     public func getCartItems() -> [CartEntity] {
         return cartRepository.fetchCartItems().map { dto in
             return CartEntity(id: dto.id, name: dto.name, quantity: dto.quantity, price: dto.price)
